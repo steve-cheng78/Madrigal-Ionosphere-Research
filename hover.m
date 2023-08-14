06199bf7c2f10ab266b8470868cf591c48927f72
Steven Cheng <stcheng@bu.edu>
2023-08-14 17:28:40 -0400

    remove all current files from git to replace with updated edits and organization by Michael Hirsch

--- hover.m
+++
@@ -1,6 +0,0 @@
-function hover (object, eventdata, sideProfile, sideProfPts, profSegs, GDALT, NE, t, pnt, vert, date, xData, yData)
-
-hoverShowCoord (object, eventdata, t, pnt, vert, date, xData, yData)
-hoverShowProf (object, eventdata, sideProfile, sideProfPts, xData, date, profSegs, GDALT, NE)
-
-end
